6a5ec93fa2f5e39ce8155d5abb4435737affb530
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-19 07:42:25 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,78 +4,67 @@
 #include <ctype.h>
 #include <string.h>
 
-// Mapping of consonants to Soundex digits
-const char SoundexMap[26] = {
-    '0', // A
-    '1', // B
-    '2', // C
-    '3', // D
-    '0', // E
-    '1', // F
-    '2', // G
-    '0', // H
-    '0', // I
-    '2', // J
-    '2', // K
-    '3', // L
-    '5', // M
-    '5', // N
-    '6', // O
-    '1', // P
-    '2', // Q
-    '2', // R
-    '3', // S
-    '0', // T
-    '0', // U
-    '0', // V
-    '3', // W
-    '0', // X
-    '0'  // Y
-};
-
-// Get the Soundex digit for a given character
+// Get the Soundex code for a given character
 char getSoundexCode(char c) {
+    static const char codeTable[26] = {
+        '0', // A
+        '1', // B
+        '2', // C
+        '3', // D
+        '0', // E
+        '1', // F
+        '2', // G
+        '0', // H
+        '0', // I
+        '2', // J
+        '2', // K
+        '3', // L
+        '5', // M
+        '5', // N
+        '0', // O
+        '1', // P
+        '2', // Q
+        '2', // R
+        '3', // S
+        '0', // T
+        '0', // U
+        '1', // V
+        '3', // W
+        '0', // X
+        '0'  // Y
+    };
+    c = toupper(c);
     if (isalpha(c)) {
-        return SoundexMap[toupper(c) - 'A'];
+        return codeTable[c - 'A'];
     }
-    return c; // Preserve non-alphabetic characters
-}
-
-// Pad the Soundex result with zeros if necessary
-void padzeros(char* soundex, int sIndex) {
-    for (; sIndex < 4; ++sIndex) {
-        soundex[sIndex] = '0';
-    }
+    return '0';
 }
 
 // Update the Soundex code array based on the new digit
-void updateSoundex(char code, char* soundex, int* sIndex, char* previous_code) {
-    if (code != '0' && code != *previous_code) {
-        soundex[(*sIndex)++] = code;
-        *previous_code = code;
-    } else if (code == '0') {
-        *previous_code = code;
+int updateSoundex(char code, int sIndex, char *soundex) {
+    if (code != '0' && (sIndex == 1 || code != soundex[sIndex - 1])) {
+        soundex[sIndex] = code;
+        return ++sIndex;
     }
+    return sIndex;
 }
 
 // Generate Soundex code from a name
-void generateSoundex(const char* name, char* soundex) {
-    int len = strlen(name);
-    if (len == 0) {
+void generateSoundex(const char *name, char *soundex) {
+    if (name == NULL || name[0] == '\0') {
         strcpy(soundex, "0000");
         return;
     }
 
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
-    char previous_code = getSoundexCode(name[0]);
 
-    for (int i = 1; i < len && sIndex < 4; ++i) {
+    for (int i = 1; name[i] != '\0' && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        updateSoundex(code, soundex, &sIndex, &previous_code);
+        sIndex = updateSoundex(code, sIndex, soundex);
     }
-
-    padzeros(soundex, sIndex);
+    
+    memset(soundex + sIndex, '0', 4 - sIndex);
     soundex[4] = '\0';
 }
 

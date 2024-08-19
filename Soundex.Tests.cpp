3e6f1ce090d2c36ef3ddd39aed1c4593b473a73d
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-19 06:50:10 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,30 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
+void generateSoundex(const char *name, char *soundex);
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generateSoundex("AX", soundex);
+  generateSoundex("AXL", soundex);
  // ASSERT_EQ(soundex,"A200");
+}
+TEST(SoudexTestsuite, positivecase)
+{
+  char soundex[5];
+  generateSoundex("Robert", soundex);
+}
+TEST(SoudexTestsuite, vowelcase)
+{
+  char soundex[5];
+  generateSoundex("Tymczak", soundex);
+}
+TEST(SoudexTestsuite, zeropadding)
+{
+  char soundex[5];
+  generateSoundex("B", soundex);
+}
+TEST(SoudexTestsuite, samearray)
+{
+  char soundex[5];
+  generateSoundex("Pfister", soundex);
 }

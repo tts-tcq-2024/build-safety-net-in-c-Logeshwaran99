b776397d59e546c77ce082093d041a7195655469
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-19 07:15:39 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,69 +1,44 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
-
-void generateSoundex(const char *name, char *soundex);
+ 
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
+//AAA
   char soundex[5];
-  generateSoundex("AXL", soundex);
-  EXPECT_STREQ(soundex,"A200");
+generateSoundex("$rrr", soundex);
+ASSERT_STREQ(soundex,"$666");
 }
-TEST(SoundexTestsuite, BasicInput) {
-    char soundex[5];
-    generateSoundex("Robert", soundex);
-    EXPECT_STREQ(soundex, "R163");
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_1) {
+//AAA
+char soundex[5];
+generateSoundex("pvt", soundex);
+ASSERT_STREQ(soundex,"P130");
 }
-
-TEST(SoundexTestsuite, InputWithSpecialCharacters) {
-    char soundex[5];
-    generateSoundex("O'Connor", soundex);
-    EXPECT_STREQ(soundex, "O252");
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_2) {
+//AAA
+  char soundex[5];
+generateSoundex("*#12hi", soundex);
+ASSERT_STREQ(soundex,"*000");
 }
-
-TEST(SoundexTestsuite, SingleCharacter) {
-    char soundex[5];
-    generateSoundex("A", soundex);
-    EXPECT_STREQ(soundex, "A000");
-}
-
-TEST(SoundexTestsuite, MixedCaseInput) {
-    char soundex[5];
-    generateSoundex("bOB", soundex);
-    EXPECT_STREQ(soundex, "B010");
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_vowels) {
+//AAA
+char soundex[5];
+generateSoundex("aeiou", soundex);
+ASSERT_STREQ(soundex,"A000");
 }
 
-TEST(SoundexTestsuite, EmptyInput) {
-    char soundex[5];
-    generateSoundex("", soundex);
-    EXPECT_STREQ(soundex, "0000");
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_space) {
+//AAA
+char soundex[5];
+generateSoundex(" ", soundex);
+ASSERT_STREQ(soundex," 000");
 }
-
-TEST(SoundexTestsuite, NonAlphabeticCharacters) {
-    char soundex[5];
-    generateSoundex("123456", soundex);
-    EXPECT_STREQ(soundex, "0000");
-}
-
-TEST(SoundexTestsuite, AllSameLetters) {
-    char soundex[5];
-    generateSoundex("AAAA", soundex);
-    EXPECT_STREQ(soundex, "A000");
-}
-
-TEST(SoundexTestsuite, ConsecutiveLettersMappingSameCode) {
-    char soundex[5];
-    generateSoundex("BFPV", soundex);
-    EXPECT_STREQ(soundex, "B111");
-}
-
-TEST(SoundexTestsuite, FullAlphabetInput) {
-    char soundex[5];
-    generateSoundex("ABCDEFGHIJKLMNOPQRSTUVWXYZ", soundex);
-    EXPECT_STREQ(soundex, "A123");
-}
-
-TEST(SoundexTestsuite, MixedCaseAndSymbols) {
-    char soundex[5];
-    generateSoundex("HeLLo_World!", soundex);
-    EXPECT_STREQ(soundex, "H450");
+ 
+TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits_3) {
+//AAA
+char soundex[5];
+generateSoundex("bcdlmnr", soundex);
+ASSERT_STREQ(soundex,"B234");
 }

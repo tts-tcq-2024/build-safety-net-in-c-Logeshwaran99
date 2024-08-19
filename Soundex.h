d8b2f9507d5ba38886926fb505dc7f2956233f3d
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-19 08:02:36 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,40 +1,47 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
- 
+
 #include <ctype.h>
 #include <string.h>
-#include <stdio.h>
- 
+
+const char SoundexMap[26] = {
+    '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
+};
+
 char getSoundexCode(char c) {
-    static const char codeTable[26] = {
-        '0', '1', '2', '3', '0', '1', '2', '0', '0','2', '2', '4', '5', '5', '0', '1', '2', '6',  '2', '3', '0', '1', '0', '2', '0', '2'     
-    };
-     c = toupper(c);
-    if (isalpha(c)) {
-        return codeTable[c - 'A'];
+    c = toupper(c);
+    if (c >= 'A' && c <= 'Z') {
+        return SoundexMap[c - 'A'];
     }
     return '0';
-}  
- 
-int updateSoundex(char code, int sIndex, char *soundex) {
-    int notZero = code != '0';
-    if (notZero) {
-        soundex[sIndex] = code;
-        return ++sIndex;
-    } 
-    soundex[sIndex] = soundex[sIndex];
-    return sIndex;
 }
+
+void padzeros(char* soundex, int sIndex) {
+    while (sIndex < 4) {
+        soundex[sIndex++] = '0';
+    }
+}
+
+void updatesoundex(char code, char* soundex, int* sIndex, char* previous_code) {
+    if (code != '0' && code != *previous_code) {
+        soundex[(*sIndex)++] = code;
+        *previous_code = code;
+    }
+}
+
 void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
- 
-     for (int i = 1; name[i] != '\0' && sIndex <=3; i++) {
+    char previous_code = getSoundexCode(name[0]);
+
+    for (int i = 1; i < len && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
-        sIndex = updateSoundex(code, sIndex, soundex); 
+        updatesoundex(code, soundex, &sIndex, &previous_code);
     }
-    memset(soundex + sIndex, '0', 4 - sIndex);
+    
+    padzeros(soundex, sIndex);
     soundex[4] = '\0';
 }
- 
+
 #endif // SOUNDEX_H

b7e7be0044ca5debc70aa6aa786d9a3f0e45abb2
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-19 07:58:48 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,47 +1,21 @@
 #ifndef SOUNDEX_H
 #define SOUNDEX_H
-
+ 
 #include <ctype.h>
 #include <string.h>
 #include <stdio.h>
-
-// Get the Soundex code for a given character
+ 
 char getSoundexCode(char c) {
     static const char codeTable[26] = {
-        '0', // A
-        '1', // B
-        '2', // C
-        '3', // D
-        '0', // E
-        '1', // F
-        '2', // G
-        '0', // H
-        '0', // I
-        '2', // J
-        '2', // K
-        '3', // L
-        '5', // M
-        '5', // N
-        '0', // O
-        '1', // P
-        '2', // Q
-        '2', // R
-        '3', // S
-        '0', // T
-        '0', // U
-        '1', // V
-        '3', // W
-        '0', // X
-        '0'  // Y
+        '0', '1', '2', '3', '0', '1', '2', '0', '0','2', '2', '4', '5', '5', '0', '1', '2', '6',  '2', '3', '0', '1', '0', '2', '0', '2'     
     };
-    c = toupper(c);
+     c = toupper(c);
     if (isalpha(c)) {
         return codeTable[c - 'A'];
     }
     return '0';
-}
-
-// Update the Soundex code array based on the new digit
+}  
+ 
 int updateSoundex(char code, int sIndex, char *soundex) {
     int notZero = code != '0';
     if (notZero) {
@@ -51,19 +25,16 @@ int updateSoundex(char code, int sIndex, char *soundex) {
     soundex[sIndex] = soundex[sIndex];
     return sIndex;
 }
-
-// Generate Soundex code from a name
 void generateSoundex(const char *name, char *soundex) {
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
-
-    for (int i = 1; name[i] != '\0' && sIndex < 4; i++) {
+ 
+     for (int i = 1; name[i] != '\0' && sIndex <=3; i++) {
         char code = getSoundexCode(name[i]);
-        sIndex = updateSoundex(code, sIndex, soundex);
+        sIndex = updateSoundex(code, sIndex, soundex); 
     }
-    
     memset(soundex + sIndex, '0', 4 - sIndex);
     soundex[4] = '\0';
 }
-
+ 
 #endif // SOUNDEX_H

774b053894aa9a9f8ad9d987c6b827dd1f4c0eef
Logeshwaran99 <84559800+Logeshwaran99@users.noreply.github.com>
2024-08-20 05:43:46 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -4,6 +4,15 @@
 #include <ctype.h>
 #include <string.h>
 
+char getFirstValidCharacter(const char *name) {
+    for (int i = 0; name[i] != '\0'; i++) {
+        if (isalpha(name[i])) {
+            return toupper(name[i]);
+        }
+    }
+    return '0';  // Return '0' if no valid character is found
+}
+
 char getSoundexCode(char c) {
     static const char soundexTable[26] = {
         '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', 
@@ -14,40 +23,25 @@ char getSoundexCode(char c) {
 }
 
 void initializeSoundex(char *soundex, char firstCharacter) {
-    soundex[0] = toupper(firstCharacter);
-    soundex[1] = soundex[2] = soundex[3] = '0';
+    soundex[0] = firstCharacter;
+    memset(&soundex[1], '0', 3);  // Set soundex[1] to soundex[3] to '0'
     soundex[4] = '\0';
 }
 
-int shouldAddToSoundex(char code, char *soundex, int sIndex) {
-    return sIndex < 4 && code != '0' && code != soundex[sIndex - 1];
-}
-
-void processCharacter(const char *name, char *soundex, int *sIndex, int i) {
-    char code = getSoundexCode(name[i]);
-    if (shouldAddToSoundex(code, soundex, *sIndex)) {
-        soundex[*sIndex] = code;
-        (*sIndex)++;
+void fillSoundexCodes(const char *name, char *soundex) {
+    int sIndex = 1;
+    for (int i = 0; name[i] != '\0' && sIndex < 4; i++) {
+        char code = getSoundexCode(name[i]);
+        if (code != '0' && code != soundex[sIndex - 1]) {
+            soundex[sIndex++] = code;
+        }
     }
 }
 
 void generateSoundex(const char *name, char *soundex) {
-    int i = 0;
-    // Skip leading non-alphabetic characters
-    while (name[i] && !isalpha(name[i])) i++;
-
-    if (!name[i]) {
-        initializeSoundex(soundex, '0');
-        return;
-    }
-
-    initializeSoundex(soundex, name[i++]);
-
-    int sIndex = 1;
-    int len = strlen(name);
-    for (; i < len; i++) {
-        processCharacter(name, soundex, &sIndex, i);
-    }
+    char firstChar = getFirstValidCharacter(name);
+    initializeSoundex(soundex, firstChar);
+    fillSoundexCodes(name, soundex);
 }
 
 #endif // SOUNDEX_H
